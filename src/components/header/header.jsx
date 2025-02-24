6148a138d68b05fdc5820f87f978dd1714055bba
Abraham2912 <abrahamlorenzo2912@gmail.com>
2025-02-24 10:13:25 +0000

    Barra de navegación

---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import "./header.css"
+import Nav from "../nav/nav.jsx"
+
+function Header() {
+  return (
+    <header>
+      <Nav/>
+      <h1>Abraham Lorenzo</h1>
+    </header>
+  );
+}
+export default Header;
